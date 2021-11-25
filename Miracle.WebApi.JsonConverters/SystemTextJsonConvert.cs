e17984897530b4e44b698444db72fdbe231a4d70
Joes <dygood@outlook.com>
2021-11-25 10:29:12 +0800

    提交

--- Miracle.WebApi.JsonConverters/SystemTextJsonConvert.cs
+++ Miracle.WebApi.JsonConverters/SystemTextJsonConvert.cs
@@ -22,14 +22,12 @@ public class SystemTextJsonConvert
 
         public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString(@"HH\:mm\:ss"));
     }
-
     public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
     {
         public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => TimeOnly.Parse(reader.GetString()!);
 
         public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString(@"HH\:mm\:ss"));
     }
-
     public class DateOnlyJsonConverter : JsonConverter<DateOnly>
     {
         public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => DateOnly.Parse(reader.GetString()!);
