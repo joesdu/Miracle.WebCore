145050edfca71546af47b6f21b5fcc932d4f3f71
Joes <dygood@outlook.com>
2021-12-19 17:31:50 +0800

    一些细微调整.

--- example.api/Controllers/WeatherForecastController.cs
+++ example.api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ namespace example.api.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+    private static readonly string[] Summaries = { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
 
     [HttpGet]
     public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
